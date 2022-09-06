25e2384f4d3420553dd3a18a79dfb260d33a844f
qzd1226 <690160142@qq.com>
2022-09-05 20:42:06 -0500

    StringBuilder

--- src/javaString/javaTest.java
+++ src/javaString/javaTest.java
@@ -1,5 +1,7 @@
 package javaString;
 
+import org.junit.jupiter.api.Test;
+
 public class javaTest {
     public static void main(String[] args) {
         // 这个例子是想证明 String类 被创造后无法被修改
@@ -11,5 +13,20 @@ public class javaTest {
         s1 = s1 + s2;
         System.out.println("改变后s1:" + s1);
         System.out.println("改变后s3:" + s3);
+    }
+    @Test
+    public void test1(){
+        StringBuilder sb = new StringBuilder(10);
+        sb.append("Runoob..");
+        System.out.println(sb);
+        sb.append("!");
+        System.out.println(sb);
+        sb.insert(8, "Java");
+        System.out.println(sb);
+        sb.delete(5,8);
+        System.out.println(sb);
+        System.out.println(sb.getClass().getTypeName());
+        String str = sb.toString();
+        System.out.println(str.getClass().getTypeName());
     }
 }
